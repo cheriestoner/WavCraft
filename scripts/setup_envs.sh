9dc66a6eaf13249778901bab3833c9f3574bd33f
Xuehua Fu <ches1550@gmail.com>
2025-08-26 21:57:00 +0800

    add guide.md

--- scripts/setup_envs.sh
+++ scripts/setup_envs.sh
@@ -1,14 +1,22 @@
 mamba env create -f venvs/audiocraft.yml --no-pip -y
+source activate AudioCraft
 pip install -r audiocraft.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+conda deactivate
 
 mamba env create -f venvs/audioldm.yml --no-pip -y
+source activate AudioLDM
 pip install -r audioldm.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+conda deactivate
 
 mamba env create -f venvs/audiosr.yml --no-pip -y
+source activate AudioSR
 pip install -r audiosr.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+conda deactivate
 
 mamba env create -f venvs/wavcraft.yml --no-pip -y
+source activate WavCraft
 pip install -r wavcraft.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+conda deactivate
 
 # Prepare third-party repos
 # Comment some of them if they are unnecessary
