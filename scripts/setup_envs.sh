bcad2b823ec5833ecabf3acc7d21805734a41b31
Xuehua Fu <ches1550@gmail.com>
2025-08-28 16:17:56 +0800

    yml + txt

--- scripts/setup_envs.sh
+++ scripts/setup_envs.sh
@@ -1,22 +1,43 @@
-mamba env create -f venvs/audiocraft.yml -y
-source activate AudioCraft
-pip install -r audiocraft.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-conda deactivate
+#!/bin/bash
 
-mamba env create -f venvs/audioldm.yml -y
-source activate AudioLDM
-pip install -r audioldm.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-conda deactivate
+# 定义环境文件列表 (yml 和 txt 成对出现)
+declare -a envs=(
+  "audiocraft"
+  "audioldm"
+  "audiosr"
+  "wavcraft"
+)
 
-mamba env create -f venvs/audiosr.yml -y
-source activate AudioSR
-pip install -r audiosr.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-conda deactivate
+for name in "${envs[@]}"; do
+    yml_file="venvs/${name}.yml"
+    txt_file="venvs/${name}.txt"
 
-mamba env create -f venvs/wavcraft.yml -y
-source activate WavCraft
-pip install -r wavcraft.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
-conda deactivate
+    # 读取 yml 中的环境名
+    ENV_NAME=$(grep '^name:' "$yml_file" | awk '{print $2}')
+
+    echo "==============================="
+    echo "Processing environment: $ENV_NAME"
+    echo "==============================="
+
+    # 判断环境是否已存在
+    if conda env list | grep -q "^\s*${ENV_NAME}\s"; then
+        echo "Environment '${ENV_NAME}' already exists. Updating..."
+        mamba env update -f "$yml_file" --prune
+    else
+        echo "Environment '${ENV_NAME}' does not exist. Creating..."
+        mamba env create -f "$yml_file" -y
+    fi
+
+    # 用 pip 安装额外依赖
+    if [ -f "$txt_file" ]; then
+        echo "Installing pip dependencies for ${ENV_NAME}..."
+        source activate "$ENV_NAME" 
+        pip install -r "$txt_file" -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+        conda deactivate
+    fi
+done
+
+echo "All environments processed."
 
 # Prepare third-party repos
 # Comment some of them if they are unnecessary
